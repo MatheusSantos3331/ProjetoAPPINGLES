db9c482648b149c346d1d85d3d0d93e1dbdb6db4
MATHEUS FRANCISCO FERREIRA DOS SANTOS <matheus111111111111@gmail.com>
2023-09-01 22:47:44 -0300

    a
    a

--- mayag/App.js
+++ mayag/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>magogda da a bunda</Text>
+      <Button>Gay</Button>
       <StatusBar style="auto" />
     </View>
   );
